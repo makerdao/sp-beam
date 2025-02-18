b34125508df5a72030576cd0537d477e01230096
oddaf <106770775+oddaf@users.noreply.github.com>
2025-02-18 00:21:25 -0300

    chore: forge fmt

--- src/DSPC.t.sol
+++ src/DSPC.t.sol
@@ -139,7 +139,7 @@ contract DSPCTest is DssTest {
         dspc.file("bad", 2);
 
         vm.expectRevert("DSPC/file-unrecognized-param");
-        dspc.file("unknown", 1);     
+        dspc.file("unknown", 1);
 
         vm.stopPrank();
     }
@@ -167,7 +167,7 @@ contract DSPCTest is DssTest {
         dspc.file(ILK, "unknown", 100);
 
         vm.expectRevert("DSPC/invalid-value");
-        dspc.file(ILK, "max", uint256(type(uint16).max) + 1);           
+        dspc.file(ILK, "max", uint256(type(uint16).max) + 1);
 
         vm.stopPrank();
     }
